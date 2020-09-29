6596da6007ae48d2667e232f23bf062462fae871
Aryeh Kobrisky <aryeh dot kobrinsky at gmail>
2020-09-29 12:19:57 -0400

    google oauth complete; router redirection based on oauth added

--- server/server.js
+++ server/server.js
@@ -2,15 +2,35 @@ const express = require("express");
 const path = require("path");
 const PORT = 3333;
 const app = express();
+const cookieParser = require("cookie-parser");
+const authController = require("./controllers/authController");
+const cookieController = require("./controllers/cookieController");
+
 require("dotenv").config();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/build", express.static(path.join(__dirname, "../build")));
 // app.use(express.static("client"));
 
-app.get("/success", (req, res) => {
-  res.status(200).send("hi there");
+app.get("/login", authController.oauth, (req, res) => {
+  return res.redirect(res.locals.url);
 });
+
+app.get("/game", cookieController.hasCookie, (req, res) => {
+  // add middleware to check for SSID cookie
+  res.status(200).sendFile(path.resolve(__dirname, "../client/index.html"));
+});
+
+app.get(
+  "/success",
+  authController.onSuccess,
+  cookieController.setSSIDcookie,
+  (req, res) => {
+    // redirect to a page that will then check if there is an SSID cookie called 'user' ()
+    res.status(200).redirect("/game");
+  }
+);
 
 app.get("/", (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, "../client/index.html"));
